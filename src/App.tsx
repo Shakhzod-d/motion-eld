599df86c417c071fb04492c9e2f749c43cf2613c
bekruz-pardayev <behruzp330@gmail.com>
2024-08-29 18:04:19 +0500

    dak-mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import { Login } from "./pages";
+import { Button, ConfigProvider, theme } from "antd";
+import { useState } from "react";
+// import { dark, light } from "./utils";
 function App() {
+  const [themeValue, setTheme] = useState<string>("light");
+  const { defaultAlgorithm, darkAlgorithm } = theme;
   return (
     <>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <ConfigProvider theme={{ algorithm: themeValue=="light"?defaultAlgorithm:darkAlgorithm}}>
+        <Button onClick={()=>setTheme(e=>e=="light"?"dark":"light")}>{themeValue=="dark"?"light":"dark"}</Button>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </ConfigProvider>
     </>
   );
 }
---
+++ src/utils/index.ts
@@ -0,0 +1,8 @@
+export const dark = {
+  colorPrimary: "gray",
+  colorText: "#fff",
+};
+export const light ={
+    colorPrimary: "#f1f1f1f",
+    colorText: "#000",   
+}
