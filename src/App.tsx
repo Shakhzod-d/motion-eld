be70a1dcfa9a6b38d7f9b510d64b752a8fe9f39a
Diyorbek Burxonov <diyorburxonov0007@gmail.com>
2024-08-29 18:26:37 +0500

    create company page and navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,36 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-<<<<<<< HEAD
-import Login from "./pages/Login";
 import { Navbar } from "./components/shared";
 import { Company } from "./pages";
-=======
 import { Login } from "./pages";
 import { Button, ConfigProvider, theme } from "antd";
 import { useState } from "react";
 // import { dark, light } from "./utils";
->>>>>>> 599df86c417c071fb04492c9e2f749c43cf2613c
 function App() {
   const [themeValue, setTheme] = useState<string>("light");
   const { defaultAlgorithm, darkAlgorithm } = theme;
   return (
     <>
-<<<<<<< HEAD
-      {/* <Navbar /> */}
       <Routes>
+        <Route path="login" element={<Login />} />
         <Route path="/" element={<Navbar />}>
-          <Route path="login" element={<Login />} />
           <Route path="company" element={<Company />} />
         </Route>
       </Routes>
-=======
-      <ConfigProvider theme={{ algorithm: themeValue=="light"?defaultAlgorithm:darkAlgorithm}}>
-        <Button onClick={()=>setTheme(e=>e=="light"?"dark":"light")}>{themeValue=="dark"?"light":"dark"}</Button>
+      <ConfigProvider
+        theme={{
+          algorithm: themeValue == "light" ? defaultAlgorithm : darkAlgorithm,
+        }}
+      >
+        <Button
+          onClick={() => setTheme((e) => (e == "light" ? "dark" : "light"))}
+        >
+          {themeValue == "dark" ? "light" : "dark"}
+        </Button>
         <Routes>
           <Route path="/login" element={<Login />} />
         </Routes>
       </ConfigProvider>
->>>>>>> 599df86c417c071fb04492c9e2f749c43cf2613c
     </>
   );
 }
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,5 +1,2 @@
-<<<<<<< HEAD
 export { Company } from "./company/company";
-=======
-export  {Login} from "./Login/Login"
->>>>>>> 599df86c417c071fb04492c9e2f749c43cf2613c
+export { Login } from "./Login/Login";
