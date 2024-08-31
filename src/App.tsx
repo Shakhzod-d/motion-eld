7e22e079b38fe7dd5f3ea3ecd53f40b2140b4816
bekruz-pardayev <behruzp330@gmail.com>
2024-09-01 00:33:04 +0500

    resolve merge conflits

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,9 @@ function App() {
         <Route path="/" element={<Layout />}>
           <Route index element={<Dashboard />} />
           <Route path="company" element={<Company />} />
-<<<<<<< HEAD
-          <Route path="logs" element={<Logs />} />
-
-=======
           <Route path="units" element={<Units />} />
           <Route path="ifta-reports" element={<IftaReports />} />
           <Route path="manage-company" element={<ManageCompany />} />
->>>>>>> main
         </Route>
         <Route path="login" element={<Login />} />
       </Routes>
