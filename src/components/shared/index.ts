c64524519168840070d6f06e70d35c80be27c116
bekruz-pardayev <behruzp330@gmail.com>
2024-08-31 16:36:44 +0500

    file name

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -1,4 +1,4 @@
 export { ViolationsChart } from "./violations-chart";
 export { ProgressBar } from "./progress-bar";
 export { Sidebar } from "./sidebar";
-export { DriversInfoTable } from "./driversInfoTable";
+export { DriversInfoTable } from "./DriversInfoTable";
