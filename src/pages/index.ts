d505e9140facf337f4a15919b800c7342c48015b
Shakhzod <forworkingappsinmylaptop@gmail.com>
2024-09-10 21:47:31 +0500

    change to uppercase

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,10 +1,11 @@
 export { Company } from "./company/company";
-export { Login } from "./login";
+// export { Login } from "./login";
+export { Login } from "./Login";
 export { Units } from "./units/units";
 export { Drivers } from "./drivers/drivers";
 export { IftaReports } from "./ifta-reports/ifta-reports";
 export { ManageCompany } from "./manage-company/manage-company";
-export { Dashboard } from "./dashboard";
+export { Dashboard } from "./Dashboard";
 export { Transfer } from "./transfer/transfer";
 export { Logs } from "./logs";
 export { LogsMap } from "./logs-map";
@@ -13,4 +14,4 @@ export { LogsLog } from "./logs-log";
 export { Violation } from "./violation";
 export { Trackings } from "./trackings";
 export { DriversInfo } from "./drivers-info";
-export {Users} from "./users-page" 
+export { Users } from "./users-page";
